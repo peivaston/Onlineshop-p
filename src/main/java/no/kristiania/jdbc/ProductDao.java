49556699aae852bd4886f04f4a703c014ef320de
peivaston <peivaston@hotmail.com>
2019-10-22 17:23:51 +0200

    Implement ProdoctDao with Jdbc

--- src/main/java/no/kristiania/jdbc/ProductDao.java
+++ src/main/java/no/kristiania/jdbc/ProductDao.java
@@ -1,18 +1,47 @@
 package no.kristiania.jdbc;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
+
 import java.util.List;
 
 public class ProductDao {
-    private List<String> products = new ArrayList<>();
 
 
-    public void insertProduct(String productName) {
-        products.add(productName);
+    private DataSource dataSource;
+
+    public ProductDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
-    public List<String> listAll() {
-        return products;
+
+    public void insertProduct(String productName) throws SQLException {
+        try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(
+                    "insert into products (name) values (?)");
+            statement.setString(1, productName);
+            statement.executeUpdate();
+        }
+    }
+
+    public List<String> listAll() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "select * from products"
+            )) {
+                try (ResultSet rs = statement.executeQuery()) {
+                    List<String> result = new ArrayList<>();
+
+                    while (rs.next()) {
+                        result.add(rs.getString("name"));
+                    }
+                    return result;
+                }
+            }
+        }
     }
 }
--- src/test/java/no/kristiania/jdbc/OnlineshopTest.java
+++ src/test/java/no/kristiania/jdbc/OnlineshopTest.java
@@ -1,7 +1,9 @@
 package no.kristiania.jdbc;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,8 +11,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class OnlineshopTest {
 
     @Test
-    void shouldRetrieveStoredProduct() {
-        ProductDao dao = new ProductDao();
+    void shouldRetrieveStoredProduct() throws SQLException {
+        JdbcDataSource datasource = new JdbcDataSource();
+        datasource.setURL("jdbc:h2:mem:test");
+
+        datasource.getConnection().createStatement().executeUpdate(
+                "create table products (name varchar(100))"
+        );
+
+
+        ProductDao dao = new ProductDao(datasource);
         String productName = pickOne(new String[] { "Apples", "Bananas", "Coconuts", "Dates"});
         dao.insertProduct(productName);
         assertThat(dao.listAll()).contains(productName);
